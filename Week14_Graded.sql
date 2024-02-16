2fcad48be07a0663b6845420e87af2c7d6000d2a
essvaran <113485232+essvaran@users.noreply.github.com>
2024-02-16 12:46:44 +0530

    Add files via upload

---
+++ Week14_Graded.sql
@@ -0,0 +1,12 @@
+-- Create a database
+create database week14;
+
+use week14;
+
+show tables;
+
+drop table hibernate_sequence;
+drop table employee;
+
+select * from hibernate_sequence;
+select * from employee;
